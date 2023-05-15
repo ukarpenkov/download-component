52e03b3e66d633a9b17eabd74bfa657873d4556b
ukarpenkov <truloveu@gmail.com>
2023-05-15 18:29:34 +0300

    first commit

--- js-download-component/index.js
+++ js-download-component/index.js
@@ -2,7 +2,9 @@ const wrapper = document.querySelector('.progress-bar-wrapper')
 const progressBarContainer = document.createElement('div')
 const progressBar = document.createElement('div')
 const controlButton = document.createElement('button')
-const buttonText = document.createTextNode("stop")
+const stopButton = document.createElement('button')
+const buttonText = document.createTextNode("start")
+const stopButtonText = document.createTextNode("stop")
 const progressValue = document.createElement('h1')
 let barWidth = 0
 let isLoad = true
@@ -10,13 +12,16 @@ let isLoad = true
 progressBarContainer.className = "progress-bar-container"
 progressBar.className = "progress-bar"
 controlButton.className = "progress-bar-btn"
+stopButton.className = "progress-bar-btn"
 progressValue.className = "progress-bar-value"
 
 controlButton.appendChild(buttonText)
+stopButton.appendChild(stopButtonText)
 progressBarContainer.append(progressBar)
 wrapper.append(progressValue)
 wrapper.append(progressBarContainer)
 wrapper.append(controlButton)
+wrapper.append(stopButton)
 
 function progress(e) {
     e.preventDefault()
@@ -33,21 +38,29 @@ function startStopLoading() {
         else {
             clearInterval(progressBarInterval)
         }
-    }, 100)
+    }, 10)
 }
 
-function buttonHadler(e) {
-    if (isLoad === true) {
-        controlButton.textContent = 'start'
-        isLoad = false
-    }
-    else {
-        controlButton.textContent = 'stop'
+function stopButtonHadler(e) {
+    if (barWidth === 100) {
+        barWidth = 0
         isLoad = true
+        startStopLoading()
     }
+
+    if (isLoad === true) {
+        return
+    }
+
+    isLoad = true
+    startStopLoading()
+}
+function startButtonHadler(e) {
+    isLoad = false
     startStopLoading()
 }
 
 
 window.onload = (event) => progress(event)
-controlButton.onclick = (event) => buttonHadler(event)
+controlButton.onclick = (event) => stopButtonHadler(event)
+stopButton.onclick = (event) => startButtonHadler(event)
