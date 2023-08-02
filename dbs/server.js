c8b7f525736b09a6e69eef4f095f274963e19133
ukarpenkov <truloveu@gmail.com>
2023-08-02 15:09:02 +0300

    mongo

--- dbs/server.js
+++ dbs/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Movie = require('./models/movie')
+const movieRouters = require('./routes/movie-routes')
 
 
 const PORT = 3000
@@ -8,6 +9,7 @@ const URL = 'mongodb://localhost:27017/moviebox'
 
 const app = express()
 app.use(express.json())
+app.use(movieRouters)
 
 mongoose
   .connect(URL)
@@ -19,68 +21,3 @@ app.listen(PORT, (err) => {
 })
 
 
-app.get(
-  '/movies',
-  (req, res) => {
-    Movie.find().sort({ title: 1 })
-      .then((movies) => {
-        res.status(200).json(movies)
-      }).catch(() => {
-        res.status(500).json({ error: 'smth wrong' })
-      })
-
-  }
-)
-
-const handleError = (res, error) => {
-  res.status(500).json({ error })
-}
-
-app.get('/movies/:id', (req, res) => {
-
-  Movie
-    .findById(req.params.id)
-    .then((movie) => {
-      res
-        .status(200)
-        .json(movie)
-    }).catch(() => handleError(res, 'smth wrong'))
-
-})
-
-
-app.delete('/movies/:id', (req, res) => {
-  Movie
-    .findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res
-        .status(200)
-        .json(result)
-    }).catch(() => handleError(res, 'smth wrong'))
-})
-
-
-app.post('/movies/', (req, res) => {
-  const movie = new Movie(req.body)
-  movie
-    .save()
-    .then((result) => {
-      res
-        .status(201)
-        .json(result)
-    }).catch(() => handleError(res, 'smth wrong'))
-
-})
-
-
-app.patch('/movies/:id', (req, res) => {
-
-  Movie
-    .findByIdAndUpdate(req.params.id, req.body)
-    .then((result) => {
-      res
-        .status(200)
-        .json(result)
-    }).catch(() => handleError(res, 'smth wrong'))
-
-})
--- random-сafe/random-cafe/src/App.vue
+++ random-сafe/random-cafe/src/App.vue
@@ -1,18 +1,48 @@
 <script setup>
 import { computed, reactive, ref } from 'vue'
+import axios from 'axios'
 
 const review = reactive({
   author: '',
   stars: null,
   text: '',
-  photos: [],
+  photo: null,
   isRecomended: true,
+})
+
+const previewFilePath = computed(() => {
+  if (review.photo) {
+    return URL.createObjectURL(review.photo)
+  }
+
+  return '#'
 })
 
 const stars = [1, 2, 3, 4, 5]
 
 const submit = () => {
   console.log('sub!')
+
+  axios
+    .post('/api/review', review, {
+      headers: {
+        'Content-Type': 'multipart/from-data',
+      },
+    })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+    .finally(() => {
+      console.log('end')
+    })
+}
+
+const uploadFile = (e) => {
+  const [file] = e.target.files
+  review.photo = file
 }
 </script>
 
@@ -34,7 +64,15 @@ const submit = () => {
 
     <h4>Оценка</h4>
     <div v-for="star in stars" :key="star" class="form-check">
-      <input class="form-check-input" type="checkbox" :id="`star${star}`" />
+      <input
+        class="form-check-input"
+        type="checkbox"
+        v-model="review.stars"
+        :value="star"
+        :true-value="star"
+        :false-value="null"
+        :id="`star${star}`"
+      />
       <label class="form-check-label" :for="`star${star}`">
         {{ star }}
       </label>
@@ -42,15 +80,29 @@ const submit = () => {
 
     <div class="mt-3 mb-5">
       <label class="form-label">Фото</label>
-      <input class="form-control" type="file" />
+      <input class="form-control" type="file" @change="uploadFile" />
+
+      <img :src="previewFilePath" alt="" class="w-50 h-50 mt-5" />
     </div>
 
     <div class="form-check">
-      <input class="form-check-input" type="radio" id="adv1" />
+      <input
+        class="form-check-input"
+        type="radio"
+        id="adv1"
+        v-model="review.isRecomended"
+        :value="true"
+      />
       <label class="form-check-label" for="adv1"> Не советую </label>
     </div>
     <div class="form-check">
-      <input class="form-check-input" type="radio" id="adv2" checked />
+      <input
+        class="form-check-input"
+        type="radio"
+        id="adv2"
+        v-model="review.isRecomended"
+        :value="false"
+      />
       <label class="form-check-label" for="adv2"> Советую </label>
     </div>
 
