cc181d1f3e3415bf9448723c622e4d3802a92d1e
ukarpenkov <truloveu@gmail.com>
2023-08-03 13:37:52 +0300

    mongo

--- dbs/server.js
+++ dbs/server.js
@@ -5,14 +5,14 @@ const movieRouters = require('./routes/movie-routes')
 
 
 const PORT = 3000
-const URL = 'mongodb://localhost:27017/moviebox'
+const URL = 'mongodb+srv://velvorin:salamfu6@cluster0.god1w0i.mongodb.net/moviebox?retryWrites=true&w=majority'
 
 const app = express()
 app.use(express.json())
 app.use(movieRouters)
 
 mongoose
-  .connect(URL)
+  .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(console.log('conn with mongoose'))
   .catch(err => console.log(`DB connection error ${err}`))
 
